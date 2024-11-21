d971e1692bd4f44ade0d79bafd474de16e91ae4d
Glazachev Dmitry <GlazachevD@gmail.com>
2024-11-21 13:57:02 +0300

    add styles and articles

---
+++ client/src/components/publication.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div v-if="small">{{ publication.to_short_link_output() }}</div>
+        <div v-if="large">{{ publication.to_long_styled_output() }}</div>
+        <span v-if="links" 
+            v-for="(link, i) in publication.links" :key="i"
+            class="link">
+            <span class="link_prefix">{{ link.prefix }}</span>
+            <a :href="link.url" target="_blank">{{ link.display_name }}</a>
+        </span>
+    </div>
+  </template>
+  
+<script setup lang="ts">
+import {Publication} from '@/model/articles'
+
+
+defineProps<{
+    publication: Publication
+    small: boolean
+    large: boolean
+    links: boolean
+}>()
+</script>
+  
+
+<style scoped lang="scss">
+.link{
+    margin-right: 16px;
+    .link_prefix{
+
+    }
+    .link_prefix::after{
+        content:": ";
+    }
+}
+</style>
--- client/src/pages/articles.vue
+++ client/src/pages/articles.vue
@@ -1,3 +1,25 @@
 <template>
-    <div>ARTICLES</div>
+    <div class="flex_row_center_top">
+
+        <div class="flex_col_top_left" style="max-width: 900px;">
+            <h1>{{ $t("articles.articles") }}</h1>
+            <div class="article_full_reference" v-for="(publ, i) in publications" :key="i">
+                <Publication :publication="publ" :small="false" :large="true" :links="true"/>
+            </div>
+        </div>
+    </div> 
 </template>
+
+<script lang="ts" setup>
+
+import { publications } from "@/model/articles"
+
+</script>
+
+<style scoped lang="scss">
+  .article_full_reference{
+        margin-top: 15px;
+        margin-bottom: 15px;
+    }
+   
+</style>
